41d066ff2d495c2f40cb1f1843e102b49e4e5d05
Tim McCabe <me@timmcca.be>
2022-10-27 22:01:55 -0400

    extend install script

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
+
+cp -r ~/arch-setup /mnt/root
 
 # 2.1
 pacman -S --noconfirm wget
@@ -23,3 +25,19 @@ hwclock --systohc
 
 # 3.4 (end)
 locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+
+# 3.5
+echo tim > /etc/hostname
+
+# 3.6
+mkinitcpio -P
+
+# 3.7
+echo "set the root password"
+passwd
+
+# 3.8
+cd root
+echo 'set up a bootloader, reboot into the system, then run:'
+echo '$ ~/arch-setup/setup.sh'
