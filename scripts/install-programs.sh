c00c1265840c2f72905d51f32c834b92510bcb53
Tim McCabe <me@timmcca.be>
2022-10-29 19:01:33 -0400

    add script to install programs

---
+++ scripts/install-programs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pacman -S --noconfirm --needed \
+    firefox \
+    gparted \
+    ntfs-3g
--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 
 ~/arch-setup/scripts/enable-multilib.sh
 ~/arch-setup/scripts/set-up-gui.sh
+~/arch-setup/scripts/install-programs.sh
